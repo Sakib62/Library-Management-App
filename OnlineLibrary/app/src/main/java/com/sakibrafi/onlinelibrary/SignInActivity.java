5c9fb4ec45fef3dc10022eb4897566de8069fb83
Safayet Rafi <safayetrafi461@gmail.com>
2023-02-17 22:04:03 +0600

    User Home Activity Added

--- OnlineLibrary/app/src/main/java/com/sakibrafi/onlinelibrary/SignInActivity.java
+++ OnlineLibrary/app/src/main/java/com/sakibrafi/onlinelibrary/SignInActivity.java
@@ -2,7 +2,11 @@ package com.sakibrafi.onlinelibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Button logIn = (Button) findViewById(R.id.logIn);
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    openUserHomeActivity();
+                }
+                catch (Exception e){
+                    Toast.makeText(SignInActivity.this,"Please Enter Valid Information",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void openUserHomeActivity()
+    {
+        Intent intent = new Intent(this, UserHomeActivity.class);
+        startActivity(intent);
     }
 }
--- OnlineLibrary/app/src/main/java/com/sakibrafi/onlinelibrary/SignUpActivity.java
+++ OnlineLibrary/app/src/main/java/com/sakibrafi/onlinelibrary/SignUpActivity.java
@@ -2,7 +2,11 @@ package com.sakibrafi.onlinelibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button register = (Button) findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    openUserHomeActivity();
+                }
+                catch (Exception e){
+                    Toast.makeText(SignUpActivity.this,"Please Enter Valid Information",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void openUserHomeActivity()
+    {
+        Intent intent = new Intent(this, UserHomeActivity.class);
+        startActivity(intent);
     }
 }
